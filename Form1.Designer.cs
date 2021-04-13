44eb812ec67a4d32cf5d11a67794de3274de8cd0
NobleProgrammer <62210199+NobleProgrammer@users.noreply.github.com>
2021-04-13 23:43:55 +0300

    Files upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace DigitalTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.lblDayName = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scheduleBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStopRing = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDefault = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnRepeat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(216)))), ((int)(((byte)(255)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(10, 40);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Maximum = 60;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(75)))));
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(137)))), ((int)(((byte)(255)))));
+            this.circularProgressBar1.ProgressWidth = 15;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar1.Size = new System.Drawing.Size(224, 220);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(216)))), ((int)(((byte)(255)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "00";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.White;
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 0;
+            this.circularProgressBar1.Text = "00:00:00";
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 60;
+            this.circularProgressBar1.Click += new System.EventHandler(this.circularProgressBar1_Click);
+            // 
+            // lblDayName
+            // 
+            this.lblDayName.AutoSize = true;
+            this.lblDayName.BackColor = System.Drawing.Color.Transparent;
+            this.lblDayName.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.lblDayName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(124)))), ((int)(((byte)(65)))));
+            this.lblDayName.Location = new System.Drawing.Point(86, 174);
+            this.lblDayName.Name = "lblDayName";
+            this.lblDayName.Size = new System.Drawing.Size(66, 17);
+            this.lblDayName.TabIndex = 2;
+            this.lblDayName.Text = "Thursday";
+            this.lblDayName.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(124)))), ((int)(((byte)(65)))));
+            this.lblDate.Location = new System.Drawing.Point(84, 201);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(82, 17);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "01/01/2019";
+            this.lblDate.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 72);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(214, 24);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(124)))), ((int)(((byte)(65)))));
+            this.label1.Location = new System.Drawing.Point(3, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Schedule a time : ";
+            // 
+            // scheduleBtn
+            // 
+            this.scheduleBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.scheduleBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(49)))), ((int)(((byte)(143)))));
+            this.scheduleBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.scheduleBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(49)))), ((int)(((byte)(143)))));
+            this.scheduleBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scheduleBtn.Location = new System.Drawing.Point(6, 109);
+            this.scheduleBtn.Name = "scheduleBtn";
+            this.scheduleBtn.Size = new System.Drawing.Size(114, 32);
+            this.scheduleBtn.TabIndex = 9;
+            this.scheduleBtn.Text = "Schedule";
+            this.scheduleBtn.UseVisualStyleBackColor = true;
+            this.scheduleBtn.Click += new System.EventHandler(this.scheduleBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnRepeat);
+            this.groupBox1.Controls.Add(this.btnStopRing);
+            this.groupBox1.Controls.Add(this.scheduleBtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(129)))), ((int)(((byte)(204)))));
+            this.groupBox1.Location = new System.Drawing.Point(267, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(226, 216);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Schedule Settings";
+            // 
+            // btnStopRing
+            // 
+            this.btnStopRing.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStopRing.Location = new System.Drawing.Point(6, 157);
+            this.btnStopRing.Name = "btnStopRing";
+            this.btnStopRing.Size = new System.Drawing.Size(101, 32);
+            this.btnStopRing.TabIndex = 10;
+            this.btnStopRing.Text = "Stop Ringing";
+            this.btnStopRing.UseVisualStyleBackColor = true;
+            this.btnStopRing.Click += new System.EventHandler(this.btnStopRing_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.btnDefault);
+            this.groupBox2.Controls.Add(this.pathTextBox);
+            this.groupBox2.Controls.Add(this.browseBtn);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(129)))), ((int)(((byte)(204)))));
+            this.groupBox2.Location = new System.Drawing.Point(521, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(226, 216);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Audio Settings";
+            // 
+            // btnDefault
+            // 
+            this.btnDefault.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDefault.Location = new System.Drawing.Point(125, 76);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(94, 23);
+            this.btnDefault.TabIndex = 11;
+            this.btnDefault.Text = "Default";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            this.btnDefault.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(6, 114);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.ReadOnly = true;
+            this.pathTextBox.Size = new System.Drawing.Size(213, 24);
+            this.pathTextBox.TabIndex = 10;
+            this.pathTextBox.TextChanged += new System.EventHandler(this.pathTextBox_TextChanged);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.browseBtn.Location = new System.Drawing.Point(6, 76);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(114, 23);
+            this.browseBtn.TabIndex = 9;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(124)))), ((int)(((byte)(65)))));
+            this.label2.Location = new System.Drawing.Point(3, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Choose alarm sound [Optional] :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(717, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 31);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(681, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 31);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRepeat.Location = new System.Drawing.Point(113, 157);
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(101, 32);
+            this.btnRepeat.TabIndex = 11;
+            this.btnRepeat.Text = "Repeat";
+            this.btnRepeat.UseVisualStyleBackColor = true;
+            this.btnRepeat.Click += new System.EventHandler(this.btnRepeat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(36)))), ((int)(((byte)(74)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(760, 274);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDayName);
+            this.Controls.Add(this.circularProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Digital Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private System.Windows.Forms.Label lblDayName;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button scheduleBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStopRing;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnDefault;
+        private System.Windows.Forms.Button btnRepeat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace DigitalTimer
+{
+    public partial class Form1 : Form
+    {
+        //Data Members : 
+        System.Timers.Timer scheduleTimer = new System.Timers.Timer();
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        String filePath = "";
+        bool isBrowsed = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // This is to display the Circular Clock.
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            // Now here, we need to make a new timer for the schedule.
+            scheduleTimer.Interval = 1000;
+            scheduleTimer.Elapsed += ScheduleTimer_Elapsed;
+        }
+
+        //Best way to make form moves lol...
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+
+        //This method is for the schedule time...
+        private void ScheduleTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime userTime = dateTimePicker1.Value;
+            //Now check if the current time is equal to the user time he scheduled..
+            if (currentTime.Hour == userTime.Hour && currentTime.Minute == userTime.Minute && currentTime.Second == userTime.Second)
+            {
+                //Stop the alarm from calculating
+                scheduleTimer.Stop();
+                //Now Play a sound here ....
+                try
+                {
+                    if (isBrowsed)
+                    {
+                        player.URL = filePath;
+                    }
+                    else
+                    {
+                        player.URL = "electronic_music.mp3";
+                        
+                    }
+                    player.controls.play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        //This method for the clock timer display ...
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            circularProgressBar1.Invoke((MethodInvoker)delegate
+           {
+               circularProgressBar1.Text = DateTime.Now.ToString("hh:mm:ss");
+               circularProgressBar1.SubscriptText = DateTime.Now.ToString("tt");
+               circularProgressBar1.Value = Convert.ToInt32(DateTime.Now.ToString("mm"));
+               lblDayName.Text = DateTime.Now.ToString("dddd");
+               lblDate.Text = DateTime.Now.ToString("MM/dd/yyyy");
+           });
+        }
+
+        private void circularProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scheduleBtn_Click(object sender, EventArgs e)
+        {
+            scheduleTimer.Start();
+        }
+
+        private void btnStopRing_Click(object sender, EventArgs e)
+        {
+            scheduleTimer.Stop();
+            player.controls.stop();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //How to minimize ur cute application..
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.pathTextBox.Text = openFileDialog.FileName;
+                filePath = openFileDialog.FileName;
+                isBrowsed = true;
+                
+            }
+        }
+
+        private void pathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isBrowsed = false;
+            pathTextBox.Text = "electronic_music.mp3";
+        }
+
+        private void btnRepeat_Click(object sender, EventArgs e)
+        {
+            player.settings.setMode("loop", true);
+        }
+    }
+}
